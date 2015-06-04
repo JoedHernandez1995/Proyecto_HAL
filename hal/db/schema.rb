ba39a467a74d4031bf1392412e2f4b5c02bd1484
joedHernandez1995 <joedhernandez95@unitec.edu>
2015-06-04 00:27:48 -0600

    Se modificaron los estilos

--- hal/db/schema.rb
+++ hal/db/schema.rb
@@ -22,9 +22,11 @@ ActiveRecord::Schema.define(version: 20150603185848) do
     t.integer  "user_id",    limit: 4
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.string   "picture",    limit: 255
+    t.string   "image",      limit: 255
   end
 
-  add_index "profiles", ["user_id"], name: "fk_rails_31817e20d1", using: :btree
+  add_index "profiles", ["user_id"], name: "fk_rails_7ea1995573", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
